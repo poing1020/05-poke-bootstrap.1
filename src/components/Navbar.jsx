ef1898b100ffd8794e8c9cb1082ad97f3b3c2518
poing1020 <d.cassmagiacolor@hotmail.com>
2025-06-12 17:24:49 -0600

    Navbar con bootstrap

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,18 @@
 const Navbar = () => {
   return (
-    <div>
-      <div>NavBar</div>
-    </div>
+    <nav className='navbar navbar-expand-sm navbar-light bg-danger d-flex justify-content-between px-4'>
+      <a className='navbar-brand' to='#'>Pokédex</a>
+
+      <ul className='navbar-nav mr-auto mt-2 mt-lg-0'>
+        <li className='nav-item px-2'>
+          <a className='nav-link' to='/'>Home</a>
+        </li>
+        <li className='nav-item px-2'>
+          <a className='nav-link' to='/about'>Acerca de Pokémon</a>
+        </li>
+      </ul>
+
+    </nav>
   )
 }
-
 export default Navbar
