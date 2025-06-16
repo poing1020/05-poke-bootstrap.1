58c7273ee3e064dd30939b723535a8f0954d63e7
poing1020 <d.cassmagiacolor@hotmail.com>
2025-06-16 16:22:53 -0600

    Primer commit funcionando

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import { NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <nav className='navbar navbar-expand-sm navbar-light bg-danger d-flex justify-content-between px-4'>
+      <NavLink className='navbar-brand' to='#'>Pokédex</NavLink>
+
+      <ul className='navbar-nav mr-auto mt-2 mt-lg-0'>
+        <li className='nav-item px-2'>
+          <NavLink className='nav-link' to='/'>Home</NavLink>
+        </li>
+        <li className='nav-item px-2'>
+          <NavLink className='nav-link' to='/about'>Acerca de Pokémon</NavLink>
+        </li>
+      </ul>
+
+    </nav>
+  )
+}
+export default Navbar
